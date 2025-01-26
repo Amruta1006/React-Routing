a657cc180e010a42387124a51247b641a29c158f
Amruta <amrutabmaskar@gmail.com>
2025-01-26 18:24:21 +0530

    routing using if-else statement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import {createRoot} from "react-dom/client";
+import ReactDom from "react-dom/client";
+import Home from "./views/Home";
+import Dashboard from "./views/Dashboard";
+import About from "./views/About";
+import Contact from "./views/Contact";
 
-const root = createRoot(document.getElementById("root"));
+
+function App() {
+  const path = window.location.pathname;
+  console.log(path);
+
+  if(path === "/") {
+    root.render (<Home />)
+  }
+
+   else if(path === "/dashboard") {
+      root.render(<Dashboard />)
+    }
+
+  else if(path === "/about") {
+      root.render(<About/>)
+    }
+
+  else if(path === "/contact") {
+      root.render(<Contact/>)
+    }
+
+  else{
+    root.render(<h1>404 Not Found</h1>)
+  }
+
+  }
+  
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
-
 <>
-<div>Amruta</div>
+<App />
 </>
 
 );
